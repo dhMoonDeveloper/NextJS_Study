da44e45b5fce38d361a401324b6331c9507dc085
DONGHYUN_PC\xlr3z <dhmoon1023@gmail.com>
2026-02-20 10:06:13 +0900

    vercel 배포

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 export const getStaticProps = async() => {
     // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수 
-
-    console.log("인덱스 페이지");
     const [allBooks, recoBooks] = await Promise.all([
       fetchBooks(),
       fetchRandomBooks(),
@@ -29,6 +28,13 @@ export default function Home({
 } : InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요" />
+      </Head>
       <div className={style.container}>
         <section>
           <h3>지금 추천하는 도서</h3>
@@ -39,6 +45,7 @@ export default function Home({
           {allBooks.map((book) => <BookItem key={book.id} {...book} />)}
         </section>
       </div> 
+    </>
   );
 }
 
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,11 +1,10 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode, useEffect, useState } from "react";
-import books from "@/mock/books.json"
 import BookItem from "@/components/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import { useRouter } from "next/router";
 import { BookData } from "@/types";
+import Head from "next/head";
 
 
 export default function Page() {
@@ -26,6 +25,11 @@ export default function Page() {
     },[q])
 
     return <div>
+            <Head>
+                <title>한입북스 - 검색결과</title>
+                <meta property="og:image" content="/thumbnail.png" />
+                <meta property="og:title" content="한입북스 - 검색결과" />
+            </Head>
         {books.map((book) => <BookItem key={book.id} {...book} />)}
     </div>
 }
